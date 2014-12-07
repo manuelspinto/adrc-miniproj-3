8c46b710cc583d7e474d48b3c0db323537c49a00
Maria Dominguez <maria@mcd.tecnico.ulisboa.pt>
2014-12-07 23:56:42 +0000

    alinea b feita! :)

--- connectivity.c
+++ connectivity.c
@@ -6,7 +6,7 @@
 
 void remove_link(int head, int tail, Node **graph){
 	Node *aux;
-
+	Node *rem;
 
 	if(graph[head]->num == tail){
 		aux = graph[head];
@@ -16,8 +16,9 @@ void remove_link(int head, int tail, Node **graph){
 	}
 
 	for(aux=graph[head]; aux->next->num != tail; aux = aux->next);
-	aux = aux->next->next;
-	free(aux->next);
+	rem = aux->next;
+	aux->next = aux->next->next;
+	free(rem);
 
 	return;
 }
@@ -45,6 +46,40 @@ void print_graph(Node**graph, int n){
 		printf("\n");
 	}
 
+}
+
+
+/*retorna um ponteiro para uma copia do grafo que descreve a rede*/
+Node ** copy_graph(Node ** network, int n){
+
+	int i;
+	Node * aux;
+	Node ** copy = (Node**) malloc((n+1)*sizeof(Node*));
+
+	for(i = 0; i <= n; i++) copy[i] = NULL;
+
+	for(i = 0; i <= n; i++)
+		for(aux = network[i]; aux != NULL; aux = aux->next)
+			new_link(i, aux->num, copy);
+
+	return copy;
+
+}
+
+/*apaga um grafo da memoria*/
+void delete_graph(Node ** graph, int n){
+
+	Node * aux;
+	int i;
+
+	for(i = 0; i <= n; i++)
+		while(graph[i] != NULL){
+			aux = graph[i];
+			graph[i] = graph[i]->next;
+			free(aux);
+		}
+
+	return;
 }
 
 void BFS(Node **graph, int n, int s, int t, int *parents){
@@ -111,19 +146,130 @@ int disjoint_paths(Node **graph,int n, int s, int t){
 		parents[i] = -1;
 		BFS(graph,n,s,t,parents);
 		printf("\n");
-		print_graph(graph, n);
+		/*print_graph(graph, n);*/
 	}
 
 	return count;
 }
 
+K * new_k(int k){
+	
+	K * new = (K*) malloc (sizeof(K));
+	new->k = k;
+	new->next = NULL;
+	new->nodes = NULL;
+
+	return new;
+}
+
+Pair * new_pair(int s, int t){
+
+	Pair * new = (Pair*) malloc (sizeof(Pair));
+	new->s = s;
+	new->t = t;
+	new->next = NULL;
+
+	return new;
+}
+
+/*retorna um apontador para a k_list com o k dado*/
+K * return_k_list(K ** k_lists, int k){
+	K * aux_b, * aux_a, * new;
+	
+	/*Ver se existe uma k_list com o k dado*/
+	/*se nao existir nenhuma k_list*/
+	if(*k_lists == NULL){
+		*k_lists = new_k(k);
+		return *k_lists;
+	}
+
+	/*ver se vamos ter de inserir um no K na primeira posicao de k_lists*/
+	if((*k_lists)->k > k){
+		new = new_k(k);
+		new->next = (*k_lists);
+		*k_lists = new;
+		return *k_lists;
+	}
+
+	/*aux_b vai ficar a apontar para a k_list que queremos ou para o sitio onde a queremos criar*/
+	for(aux_a = (*k_lists)->next, aux_b = *k_lists; aux_a != NULL && aux_a->k <= k; aux_a = aux_a->next, aux_b = aux_b->next);
+
+	if(aux_b->k == k){
+		return aux_b;
+	}
+
+	new = new_k(k);
+	new->next = aux_b->next;
+	aux_b->next = new;
+	return aux_b->next;
+}
+
+/*adicionar a lista k, o par de nos (s,t)*/
+void add_pair(K ** k_lists, int s, int t, int k){
+
+	Pair * new = new_pair(s,t);
+	K * k_list = return_k_list(k_lists, k);
+
+	new->next = k_list->nodes;
+	k_list->nodes = new;
+}
+
+void print_k_lists(K * k_lists){
+	K * aux_k;
+	Pair * aux_p;
+
+	for(aux_k = k_lists; aux_k != NULL; aux_k = aux_k->next){
+		printf("k = %d: ", aux_k->k);
+		for(aux_p = aux_k->nodes; aux_p != NULL; aux_p = aux_p->next){
+			printf(" (%d,%d)", aux_p->s,aux_p->t);
+		}
+		printf("\n");
+	}
+	
+	return;
+}
+
+
+void print_k_counts(K * k_lists){
+
+	K * aux_k;
+	Pair * aux_p;
+	int max_k, pairs_num = 0, i;
+	int * k_counts;
+
+	for(aux_k = k_lists; aux_k != NULL; aux_k = aux_k->next)
+		max_k = aux_k->k;
+
+	k_counts = (int*) malloc((max_k+1)*sizeof(int));
+
+	for(i = 0; i <= max_k; i++)
+		k_counts[i] = 0;
+
+	for(aux_k = k_lists; aux_k != NULL; aux_k = aux_k->next){
+		for(aux_p = aux_k->nodes; aux_p != NULL; aux_p = aux_p->next){
+			k_counts[aux_k->k]++;
+			pairs_num++;
+		}
+	}
+
+	printf("Printing the complementary cumulative distribution of the minimum number of links that separates one node from another...\n");
+
+	for(i = 0; i <= max_k; i++)
+		printf("k = %d: %.2f %% of the pairs of nodes\n", i, (((float) k_counts[i] / pairs_num))*100);
+
+	printf("\n");
+
+	free(k_counts);
+
+	return;
+}
 
 int main(int argc, char *argv[]){
 	FILE *fp;
 	char buff[128];
-	int n, i, head, tail, option, source, dest;
+	int n, i, j, k, head, tail, option, source, dest;
 	Node **network, **res_net;
-	
+	K * k_lists = NULL;	
 
 
 	fp = fopen(argv[1],"r");
@@ -136,27 +282,17 @@ int main(int argc, char *argv[]){
 	sscanf(buff,"%d",&n);
 
 	network = (Node**) malloc((n+1)*sizeof(Node*));
-	res_net = (Node**) malloc((n+1)*sizeof(Node*));
 	
-	for(i = 0; i < n+1 ; i++){
+	for(i = 0; i < n+1 ; i++)
 		network[i] = NULL;
-		res_net[i] = NULL;
-	}
 
   
 	while (fgets(buff, sizeof(buff), fp) != NULL ){
 	  sscanf(buff,"%d %d",&head,&tail);
 	  new_link(head, tail, network);
-	  new_link(head, tail, res_net);
 	}
 
 	print_graph(network, n);
-
-	/*BFS(res_net,n,1,6,parents);
-	for(i = 0; i<= 6; i++)
-		printf("%d tem pai %d\n",i,parents[i]);*/
-
-	/*printf("Number of Link disjoint paths = %d\n",disjoint_paths(res_net,n,1,6));*/
 
 	while(1){
 		printf("[1] Compute the minimum number of links that separates a source node from a destination node\n");
@@ -176,13 +312,30 @@ int main(int argc, char *argv[]){
 				scanf("%d", &dest);
 				if(dest > n || dest < 1) printf("\nInvalid destination node!\n");
 
-				else printf("\nNumber of Link disjoint paths from source node %d to destination node %d = %d\n\n", source, dest, disjoint_paths(res_net,n,1,6));
+				else{
+					res_net = copy_graph(network,n);
+					printf("\nNumber of Link disjoint paths from source node %d to destination node %d = %d\n\n", source, dest, disjoint_paths(res_net,n,source,dest));
+					delete_graph(res_net,n);					
+				}
 			}
-			
-
 		}
-		else if(option == 2){
+		else if(option == 2){ /*LIBERTAR MEMORIA*/
 
+			for(i = 1; i <= n; i++){
+				for(j = 1; j <= n; j++){
+					if(i != j){
+						res_net = copy_graph(network,n);
+						k = disjoint_paths(res_net,n,i,j);
+						printf("from s = %d to t = %d, k = %d\n", i, j, k);
+						add_pair(&k_lists, i, j, k);
+						delete_graph(res_net,n);
+						print_k_lists(k_lists);
+						printf("\n\n");
+					}
+				}
+			}
+
+			print_k_counts(k_lists);
 		}
 		else if(option == 3){
 
--- connectivity.h
+++ connectivity.h
@@ -6,5 +6,21 @@ typedef struct node{
 
 } Node;
 
+typedef struct pair_node{
+	int s;
+	int t;
+	struct pair_node *next;
+
+} Pair;
+
+
+typedef struct k_node{
+	int k;
+	struct k_node *next;
+	struct pair_node *nodes;
+
+} K;
+
+
 
 
